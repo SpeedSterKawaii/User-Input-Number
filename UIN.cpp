09533de61e6d8bd1f476a2e4cbe7267d4c81e842
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2022-02-02 09:08:51 -0500

    Create UIN.cpp

---
+++ UIN.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int val;
+    cout<<"Enter the Number: ";
+    cin>>val;
+    cout<<"\nThe Value is "<<val;
+    cout << "bye";
+    cout<<endl;
+    Sleep(50000)
+    return 0;
+}
